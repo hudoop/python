ef8ec3bee972b83d72b19743dd7f0f7029b3e9b9
hudoop <zhangshijieno1@163.com>
2017-04-11 23:42:03 +0800

    静态爬虫京东某个店铺

--- jdAppleMacComment.py
+++ jdAppleMacComment.py
@@ -1,22 +1,21 @@
-#coding=utf-8
-import urllib2
-import requests,json,time
-return_data=requests.get("https://club.jd.com/productpage/p-1963535701-s-0-t-1-p-2.html")
-#comment1=json.dumps(return_data.text)
-comment=json.loads(return_data.text).get('comments')
-comment1=json.dumps(comment,sort_keys=True)
-comment2=json.loads(comment1)
-for i in comment2:
-    print "评论时间："i["creationTime"]
-    print "用户昵称："+str(i["nickname"])
-    print "会员等级："+str(i["userLevelName"])
-    print "评论来源："+str(i["userClientShow"])
-    print "购买品种："+str(i["referenceName"])
-    print "评价："+str(i["content"])
-    print '\n'
-
-
-
-
-
-
+# coding=utf-8
+import requests,json,ast
+from pprint import pprint
+linlsta='https://club.jd.com/productpage/p-1958407868-s-0-t-1-p-'
+linkend='.html'
+for j in range(0,248):
+    print "正在打印第"+str(j)+"页"
+    return_data=requests.get(linlsta+str(j)+linkend).json()
+    comment1 = json.dumps(return_data, indent=4, sort_keys=True, separators=(',', ':'), encoding='utf-8',
+                          ensure_ascii=False)
+    comment = json.loads(comment1)
+    comment_list = comment['comments']
+    for i in range(0, len(comment['comments'])):
+        print comment_list[i]['creationTime']
+        print comment_list[i]['id']
+        print comment_list[i]['nickname']
+        print comment_list[i]['productColor']
+        print comment_list[i]['userLevelName']
+        print comment_list[i]['userProvince']
+        print comment_list[i]['content']
+        print "\n"
